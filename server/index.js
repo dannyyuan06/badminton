f3d8a7cae333a7882c9271b8458e52da940c695c
Danny Yuan <dannyyuan06@icloud.com>
2023-12-23 11:41:30 +0000

    added ip changes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
+const ip = require("ip")
 
 const app = express();
 const server = http.createServer(app);
@@ -18,7 +19,6 @@ io.on('connection', (socket) => {
   console.log('A user connected');
 
   socket.on('updateCounter', (newCounter) => {
-  console.log('conter updated');
   io.emit('counter', newCounter);
   });
 
@@ -29,6 +29,6 @@ io.on('connection', (socket) => {
 
 const port = process.env.PORT || 3001;
 
-server.listen(port, '192.168.137.1', () => {
-  console.log(`Server is running on port ${port}`);
+server.listen(port, ip.address(), () => {
+  console.log(`Server is running on ${ip.address()}:${port}`);
 });
